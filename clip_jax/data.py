edb144f43d8fc720b00f1d637e7a3307aea182bf
Boris Dayma <boris.dayma@gmail.com>
2022-09-21 22:31:17 +0000

    feat: read buckets

--- clip_jax/data.py
+++ clip_jax/data.py
@@ -121,7 +121,12 @@ class Dataset:
         ):
             if folder is not None:
                 # load files
-                files = [f"{Path(f)}" for f in Path(folder).glob("*.tfrecord")]
+                if "gs://" in folder:
+                    if folder[-1] != "/":
+                        folder += "/"
+                    files = tf.io.gfile.glob(f"{folder}*.tfrecord")
+                else:
+                    files = [f"{Path(f)}" for f in Path(folder).glob("*.tfrecord")]
                 assert len(files) > 0, f"No files found at folder: {folder}"
 
                 # shuffle files
@@ -135,6 +140,12 @@ class Dataset:
                     if dataset == "train"
                     else None,
                 )
+
+                # non deterministic read (faster)
+                if augment:
+                    ignore_order = tf.data.Options()
+                    ignore_order.deterministic = False
+                    ds = ds.with_options(ignore_order)
 
                 if self.multi_hosts and dataset == "train":
                     # repeat indefinitely
