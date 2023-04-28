a27d2f8c903ed85ee09490c3ecb50df57b71ab42
Boris Dayma <boris.dayma@gmail.com>
2023-04-28 22:08:49 +0000

    feat: update script

--- training/do_train.bash
+++ training/do_train.bash
@@ -10,11 +10,11 @@ WANDB_NOTES=$(cat ~/instance_name) python train.py \
     --tokenizer_name craiyon/clip/craiyon_tokenizer:v0 \
     --do_train --do_eval \
     --batch_size_per_node 4096 --gradient_accumulation_steps 1 \
-    --learning_rate 0.0001 --warmup_steps 2000 --lr_offset 0 \
+    --learning_rate 0.00003 --warmup_steps 2000 --lr_offset 0 \
     --optim distributed_shampoo --beta1 0.9 --beta2 0.99 --weight_decay 0.0 \
     --block_size_text 512 --block_size_vision 512 --nesterov \
     --graft_type rmsprop_normalized --preconditioning_compute_steps 20 \
     --mp_devices 1 --shard_shampoo_across data \
-    --activation_partitioning_dims 1 --parameter_partitioning_dims 1 \
+    --activation_partitioning_dims 1 --parameter_partitioning_dims 2 \
     --loss_type sigmoid \
-    --logging_steps 100 --eval_steps 500 --save_steps 5000 --do_test_steps 100000
+    --logging_steps 100 --eval_steps 500 --save_steps 5000 --do_test_steps 310 --do_profile
