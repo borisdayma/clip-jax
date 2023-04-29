a4498d61905518824b9ca3510160f3b9f3a0c4ce
Boris Dayma <boris.dayma@gmail.com>
2023-04-29 06:53:23 +0000

    fix: train script

--- training/do_train.bash
+++ training/do_train.bash
@@ -2,7 +2,7 @@ UUID=$(uuidgen | cut -c1-8)
 
 WANDB_NOTES=$(cat ~/instance_name) python train.py \
     --assert_TPU_available \
-    --unroll 1 \
+    --unroll 100 --gradient_checkpointing \
     --output_dir gs://craiyon_models_us_central2/clip/$UUID \
     --config_name ../configs/small-patch16.json --dtype float32 \
     --key_image webp_256 \
@@ -14,7 +14,7 @@ WANDB_NOTES=$(cat ~/instance_name) python train.py \
     --optim distributed_shampoo --beta1 0.9 --beta2 0.99 --weight_decay 0.0 \
     --block_size_text 512 --block_size_vision 512 --nesterov \
     --graft_type rmsprop_normalized --preconditioning_compute_steps 20 \
-    --mp_devices 1 --shard_shampoo_across data \
+    --mp_devices 1 --shard_shampoo_across 2d \
     --activation_partitioning_dims 1 --parameter_partitioning_dims 2 \
     --loss_type sigmoid \
-    --logging_steps 100 --eval_steps 500 --save_steps 5000 --do_test_steps 310 --do_profile
+    --logging_steps 100 --eval_steps 500 --save_steps 5000 --do_test_steps 3
