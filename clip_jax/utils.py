adb3532c6d4dda26cd6cc9cb3421d2690abc5cc5
Boris Dayma <boris.dayma@gmail.com>
2024-04-04 15:52:15 +0000

    feat(utils): unbox partition

--- clip_jax/utils.py
+++ clip_jax/utils.py
@@ -1,8 +1,9 @@
 import json
+from dataclasses import fields
 
+import flax
 import fsspec
 import jax
-from dataclasses import fields
 
 from .wandb_utils import maybe_use_artifact
 
@@ -22,3 +23,20 @@ def count_params(pytree):
 def asdict(model):
     excluded_keys = ["maxtext_mesh", "maxtext_args", "parent", "name"]
     return {f.name: getattr(model, f.name) for f in fields(model) if f.name not in excluded_keys}
+
+
+# from maxtext
+def unbox_logicallypartioned(boxed_pytree):
+    """Unboxes the flax.LogicallyPartitioned pieces
+
+    Args:
+      boxed_pytree: a pytree that includes LogicallyPartitioned
+        leaves.
+    Returns:
+      a pytree where all all LogicallyPartitioned leaves have been unboxed.
+    """
+    return jax.tree_util.tree_map(
+        lambda x: x.unbox() if isinstance(x, flax.linen.spmd.LogicallyPartitioned) else x,
+        boxed_pytree,
+        is_leaf=lambda k: isinstance(k, flax.linen.spmd.LogicallyPartitioned),
+    )
